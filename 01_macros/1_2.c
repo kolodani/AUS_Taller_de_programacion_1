9f98c04cf3be2334a29c8abcc4c8e5e30d76988c
danielvinzia <danielvinzia@gmail.com>
2022-04-27 20:46:21 -0300

    ejercicio completado

---
+++ 01_macros/1_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define MAX(X, Y) ((X) > (Y) ? (X) : (Y))
+
+int main()
+{
+    int num1, num2, a;
+
+    printf("Te dire cual es el mayor numero entre dos enteros: \n");
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &num1);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &num2);
+
+    if (num1 == num2)
+    {
+        printf("Los numeros son iguales.\n");
+        return 0;
+    }
+
+    a = MAX(num1, num2);
+
+    printf("El mayor numero entre %d y %d es %d\n", num1, num2, a);
+
+    return 0;
+}
