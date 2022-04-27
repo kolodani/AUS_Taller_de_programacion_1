0d6463b1ffbc0f42d13ac769d39b87bd78e695d5
danielvinzia <danielvinzia@gmail.com>
2022-04-27 20:58:37 -0300

    ejercicio completado

---
+++ 01_macros/1_3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define PAR(X) ((X) % 2 == 0)
+
+int main()
+{
+    int num, a;
+    
+    printf("Ingresa un numero entero para saber si es par o no: ");
+    scanf("%d", &num);
+    
+    a = PAR(num);
+    if (a == 1)
+    {
+        printf("El numero %d es par", num);
+    }
+    else
+    {
+        printf("El numero %d es impar", num);
+    }
+    return 0;
+}
