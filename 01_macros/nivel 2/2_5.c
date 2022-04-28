201fadbaeb36a64a1a3afc52057626ea6e61b9a8
danielvinzia <danielvinzia@gmail.com>
2022-04-27 21:30:30 -0300

    ejercicio completado

---
+++ 01_macros/nivel 2/2_5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define ENTRE_0_Y_100(X) ((X)>=0 && (X)<=100)
+
+int main()
+{
+    int num, a;
+    
+    printf("Ingrese un numero para seber si esta entre 0 y 100: \n");
+    printf("-> ");
+    scanf("%d", &num);
+    
+    a = ENTRE_0_Y_100(num);
+    
+    if (a==1)
+    {
+        printf("el numero %d esta entre 0 y 100\n", num);
+    }
+    else
+    {
+        printf("el numero %d no esta entre 0 y 100\n", num);
+    }
+    return 0;
+}
