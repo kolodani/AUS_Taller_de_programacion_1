02073d4cd64f70243e9ce1bed6e4431aeae2050a
danielvinzia <danielvinzia@gmail.com>
2022-05-09 19:55:17 -0300

    continuar el 5

---
+++ 02_bucles/nivel 3/N3_5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int i, j;
+    int matriz[3][3];
+    
+    printf("ingrese los valores de una matriz de 4x4\n");
+    
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            printf("> ");
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            printf("%d ", &matriz[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
