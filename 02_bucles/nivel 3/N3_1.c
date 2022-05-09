d6f5b8f3baa208f39c9fd43179c1495af5581296
danielvinzia <danielvinzia@gmail.com>
2022-05-09 18:42:00 -0300

    ejecicio listo

---
+++ 02_bucles/nivel 3/N3_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("El abecedario: \n");
+    
+    char letra = 'a';
+    char letra2 = 'A';
+    int i;
+    
+    for (i = 0; i < 26; i++)
+    {
+        printf("-%c ", letra);
+        printf("-%c ", letra2);
+        letra++;
+        letra2++;
+    }
+    
+    return 0;
+}
