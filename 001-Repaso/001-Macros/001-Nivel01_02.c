e3d64ed5d17df97bb56d6dc740c4abef21ab4e08
kolodani <danielvinzia@gmail.com>
2024-06-12 20:53:05 -0300

    Repaso, macros, nivel 1, ejercicio 2

---
+++ 001-Repaso/001-Macros/001-Nivel01_02.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define MAX(X,Y) ((X)>(Y)?(X):(Y))
+
+int main(int argc, char *argv[])
+{
+    int numero1, numero2, resultado;
+    printf("Ingrese dos numeros a comparar y recibira el maximo\n");
+    printf("Ingrese el Primer Numero a comparar:\n");
+    printf("> ");
+    scanf("%d", &numero1);
+    printf("Ingrese el Segundo Numero a comparar:\n");
+    printf("> ");
+    scanf("%d", &numero2);
+
+    if (numero1 == numero2){
+        printf("Los dos numeros son %d, osea son iguales\n", numero1);
+    } 
+    else {
+        resultado = MAX(numero1, numero2);
+        printf("El maximo entre %d y %d es: %d\n", numero1, numero2, resultado);
+    }
+    return 0;
+}
