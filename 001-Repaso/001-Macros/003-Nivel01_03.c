a2853c073c7add7463de397cd8da02beacbd9ab9
kolodani <danielvinzia@gmail.com>
2024-06-12 21:11:53 -0300

    Repaso, macros, nivel 1, ejercicio 3

---
+++ 001-Repaso/001-Macros/003-Nivel01_03.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define PAR(X) ((X) % 2 == 0)
+
+int main(int argc, char *argv[])
+{
+    int numero, a;
+
+    printf("Ingrese un numero entero y te dire si es par\n");
+    printf("> ");
+    scanf("%d", &numero);
+
+    a = PAR(numero);
+
+    if (a == 0){
+        printf("El numero ingresado es %d y es IMPAR\n", numero);
+    }
+    else if (a == 1) {
+        printf("El numero ingresado es %d y es PAR\n", numero);
+    }
+    else {
+        printf("No ingreso un numero valido, error!!!\n");
+    }
+
+    return 0;
+}
