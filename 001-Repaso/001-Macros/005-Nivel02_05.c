9ff27fe578dc0faa3e14fbecbcd053d4cdedd12d
netbook XFCE kolo <danielvinzia@gmail.com>
2024-06-13 09:38:07 -0300

    Repaso, macros, nivel 2, ejercicio 5

---
+++ 001-Repaso/001-Macros/005-Nivel02_05.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define ENTRE_0_100(X) ((X)>=0 && (X)<=100)
+
+int main(){
+    int a, numero;
+
+    printf("Ingrese un numero entero y te dire si esta entre 0 y 100\n");
+    printf("> ");
+    scanf("%d", &numero);
+
+    a = ENTRE_0_100(numero);
+
+    if (a == 1){
+        printf("El numero %d esta entre 0 y 100\n", numero);
+    }
+    else if (a == 0){
+        printf("El numero %d no esta entre 0 y 100\n", numero);
+    }
+
+    return 0;
+}
