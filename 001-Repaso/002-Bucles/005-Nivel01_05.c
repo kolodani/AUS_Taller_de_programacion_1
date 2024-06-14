db0f1e828495ab7abe8c74c274927d479bbde8a5
kolodani <danielvinzia@gmail.com>
2024-06-14 01:07:12 -0300

    Repaso, bucles, nivel 1, ejercicio 5

---
+++ 001-Repaso/002-Bucles/005-Nivel01_05.c
@@ -0,0 +1,32 @@
+// Pida dos números y multiplique todos los números comprendidos entre el primero y el segundo (inclusive).
+// Debe controlarse que el segundo número sea mayor que el primero.
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int primero, segundo, producto = 1;
+
+    printf("Ingrese dos numeros, te dare el producto de todos los enteros entre ellos\n");
+    printf("Ingrese el Primer numero\n");
+    printf("> ");
+    scanf("%d", &primero);
+    printf("Ingrese el Segundo numero\n");
+    printf("> ");
+    scanf("%d", &segundo);
+
+    if (primero > segundo)
+    {
+        printf("El primer numero que es %d es mayor que el segundo que es %d\n", primero, segundo);
+    }
+    else
+    {
+        for (int i = primero; i <= segundo; i++)
+        {
+            producto = producto * i ;
+        }
+        printf("El producto de los numeros desde %d hasta %d es: %d\n", primero, segundo, producto);
+    }
+
+    return 0;
+}
