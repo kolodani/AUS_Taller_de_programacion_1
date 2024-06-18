eb6855df5c1fedf96fbba18d1384a657ce6fe440
netbook XFCE kolo <danielvinzia@gmail.com>
2024-06-18 09:58:32 -0300

    Repaso, macros, nivel 3, ejercicio 2

---
+++ 001-Repaso/002-Bucles/014-Nivel03_02.c
@@ -0,0 +1,25 @@
+// Calcule el factorial de un número entero positvo
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int numero;
+    int factorial = 1;
+
+    printf("Ingrese un numero entero positivo y te dare su factorial!!!\n");
+    printf("> ");
+    scanf("%d", &numero);
+
+    if (numero > 0){
+        for (int i = numero; i >= 2; i--) {
+            factorial = factorial * i;
+        }
+        printf("EL factorial de %d es: %d\n", numero, factorial);
+    }
+    else {
+        printf("El entero tiene que ser mayor a cero, sino no se puede hacer el factorial\n");
+    }
+
+    return 0;
+}
