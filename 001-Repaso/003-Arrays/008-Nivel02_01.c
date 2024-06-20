b4d1baa068617d942cac2535a086f6b5b7427e42
kolodani <danielvinzia@gmail.com>
2024-06-20 18:38:05 -0300

    Repaso, arrays, nivel 2, ejercicio 8

---
+++ 001-Repaso/003-Arrays/008-Nivel02_01.c
@@ -0,0 +1,25 @@
+// Permita la caga de una array de 30 enteros aleatorios. Luego mostrarlos por pantalla.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 30
+
+int main(int argc, char const *argv[])
+{
+    int A[N];
+    srand(time(NULL));
+    printf("Arreglo random de 30 enteros:\n");
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = rand() % 11;
+        printf("%d ", A[i]);
+        if(i != N - 1)
+        {
+            printf(",");
+        }
+    }
+
+    return 0;
+}
