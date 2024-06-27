c45b7ee10b21b473ba367309afa4940605372d32
netbook XFCE kolo <danielvinzia@gmail.com>
2024-06-27 10:15:23 -0300

    Repaso, funciones, nivel 2, ejercicio 3

---
+++ 001-Repaso/004-Funciones/011-Nivel02_03.c
@@ -0,0 +1,32 @@
+// Dado un array de N enteros, crear una función para invertirlo y posteriormente mostrarlo por pantalla.
+
+#include <stdio.h>
+
+void invertido(int[], int);
+
+int main(int argc, char *argv[])
+{
+    int tama;
+
+    printf("Ingrese un tamaño para el arreglo:\n");
+    printf("> ");
+    scanf("%d", &tama);
+    int arreglo[tama];
+
+    printf("Ingrese los valores del arreglo:\n");
+    for (int i = 0; i < tama; i++) {
+        printf("valor %d:\n", i + 1);
+        printf("> ");
+        scanf("%d", &arreglo[i]);
+    }
+    
+    invertido(arreglo, tama);
+    return 0;
+}
+
+void invertido(int arreglo[], int tama){
+    printf("El arreglo invertido:\n");
+    for (int i = (tama - 1); i >= 0; i--) {
+        printf("%d ", arreglo[i]);
+    }
+}
